b9eab21702e568b1017a2faa8b36b5d23f3dba12
Ong Chansy <ongchansy13@gmail.com>
2025-03-30 14:54:07 +0700

    is it work haha

--- src/app/admin/jobs/[slug]/page.tsx
+++ src/app/admin/jobs/[slug]/page.tsx
@@ -2,9 +2,41 @@
 import JobPage from "@/components/customComponents/JobPage";
 import prisma from "@/lib/prisma";
 import AdminSidebar from "./AdminSideBar";
+import { notFound } from "next/navigation";
+import { Metadata } from "next";
+import { cache } from "react";
 
 interface PageProps {
   params: { slug: string };
+}
+
+const getJob = cache(async (slug: string) => {
+  const job = await prisma.job.findUnique({
+    where: { slug },
+  });
+
+  if (!job) notFound();
+
+  return job;
+});
+
+export async function generateStaticParams() {
+  const jobs = await prisma.job.findMany({
+    where: { approved: true },
+    select: { slug: true },
+  });
+
+  return jobs.map(({ slug }) => slug);
+}
+
+export async function generateMetadata({
+  params: { slug },
+}: PageProps): Promise<Metadata> {
+  const job = await getJob(slug);
+
+  return {
+    title: job.title,
+  };
 }
 
 export default async function Page({ params: { slug } }: PageProps) {
